44b19fdebd8e28ec49ad97269fedd16fdf60ac42
Julian Herpertz <104965515+JuliHerp@users.noreply.github.com>
2023-06-11 22:31:43 +0200

    Added German language support, still under development

--- veta/auto_self_other_item.py
+++ veta/auto_self_other_item.py
@@ -1,22 +1,40 @@
 import spacy
-nlp = spacy.load("en_core_web_sm")
 
-def attempt_auto_self_other(item) -> None:
+try:
+    nlp_en = spacy.load("en_core_web_sm")
+except:
+    print("English SpaCy not detected: https://spacy.io/usage/models/")
+try:
+    nlp_de = spacy.load("de_core_news_sm")
+except:
+    print("German SpaCy not detected: https://spacy.io/usage/models/")
+
+def attempt_auto_self_other(item, lang = "en") -> None:
 
     self_sentence = ''
     other_sentence = ''
     other_flag = False
 
     #Decompose the sentence
-    doc = nlp(item.raw_input)
+    if lang.lower() == "de" or lang.lower() == "german" or lang.lower() == "deutsch":
+        doc = nlp_de(item.raw_input)
+        selfidentifiers = ['ich']
+        verblist = ["würde", "wäre", "fühlt"]
+        subject_identifier = 'sb'
+    else:
+        doc = nlp_en(item.raw_input)
+        selfidentifiers = ["i"]
+        verblist = ["feel","be","feels","feeling"]
+        subject_identifier = 'nsubj'
+
     #Loop through the sentence components
     for token in doc:
         #When we hit a sentence subject
-        if token.dep_ == 'nsubj' and not other_flag:
+        if token.dep_ == subject_identifier and not other_flag:
             #If it is a subject not equal to i and relates to the word feel
             #Then we flip the sentence to be about the other
             #Otherwise we assume we are discussing the self
-            other_flag = (token.text.lower() != 'i') and (token.head.text in ["feel","be","feels","feeling"] )
+            other_flag = (token.text.lower() not in  selfidentifiers) and (token.head.text in  verblist)
         if other_flag:
             other_sentence += token.text + ' '
         else:
